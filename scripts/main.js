ee7d92ca0399c663648344f21e10ddaa5a0aaaaa
par1tet <mankova486@gmail.com>
2024-05-19 12:39:22 +0300

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+document.querySelector('#bt_1').onclick = bt_1
+document.querySelector('#bt_2').onclick = bt_2
+document.querySelector('#bt_3').onclick = bt_3
+
+document.querySelector('#bt_4').onclick = bt_4
+document.querySelector('#bt_5').onclick = bt_5
+document.querySelector('#bt_6').onclick = bt_6
+
+document.querySelector('#bt_7').onclick = bt_7
+document.querySelector('#bt_8').onclick = bt_8
+document.querySelector('#bt_9').onclick = bt_9
+
+document.querySelector('#bt_0').onclick = bt_0
+
+function bt_1() {
+    console.log(234)
+}
+function bt_2() {
+    console.log(234)
+}
+function bt_3() {
+    console.log(234)
+}
+
+function bt_4() {
+    console.log(234)
+}
+function bt_5() {
+    console.log(234)
+}
+function bt_6() {
+    console.log(234)
+}
+
+function bt_7() {
+    console.log(234)
+}
+function bt_8() {
+    console.log(234)
+}
+function bt_9() {
+    console.log(234)
+}
+
+function bt_0() {
+    console.log(234)
+}
